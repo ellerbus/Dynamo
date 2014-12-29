97915e8329ad0156f77e8f6015380fb56497fb9b
Stu Ellerbusch <stu.ellerbusch@gmail.com>
2014-12-29 12:01:19 -0500

    Default JSON Formatter

--- Sample/Sample.Web/App_Start/WebApiConfig.cs
+++ Sample/Sample.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace Sample.Web
 {
@@ -6,6 +10,11 @@ namespace Sample.Web
     {
         public static void Register(HttpConfiguration config)
         {
+            //  Enfore camelCasing
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Formatters.OfType<JsonMediaTypeFormatter>().First().SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             // Web API configuration and services
 
             // Web API routes
