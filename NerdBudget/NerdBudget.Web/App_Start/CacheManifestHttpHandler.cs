a971e4e6c1650afbe81fcfb1c997dfeff3056510
Stu Ellerbusch <stu.ellerbusch@gmail.com>
2015-04-23 09:57:26 -0400

    Added Smart Cache.Manifest

---
+++ NerdBudget/NerdBudget.Web/App_Start/CacheManifestHttpHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Augment;
+
+namespace NerdBudget.Web
+{
+    public class CacheManifestHttpHandler : Augment.Caching.CacheManifestHttpHandler
+    {
+        #region Static Methods
+
+        private static List<string> _filesToCache;
+
+        private static object _lock = new object();
+
+        #endregion
+
+        protected override IEnumerable<string> GetCacheSectionFiles()
+        {
+            return GetNerdBudgetFiles();
+        }
+
+        private IEnumerable<string> GetNerdBudgetFiles()
+        {
+            lock (_lock)
+            {
+                if (_filesToCache == null)
+                {
+                    _filesToCache = new List<string>();
+
+                    foreach (string file in GetUrlPathToFiles("~/App"))
+                    {
+                        _filesToCache.Add(file);
+                    }
+
+                    foreach (string file in GetUrlPathToFiles("~/Content"))
+                    {
+                        _filesToCache.Add(file);
+                    }
+                }
+
+                return _filesToCache;
+            }
+        }
+
+        protected override IEnumerable<string> GetNetworkSectionFiles()
+        {
+            yield break;
+        }
+
+        public override string Version
+        {
+            get
+            {
+                if (_version.IsNullOrEmpty())
+                {
+                    _version = GetType().Assembly.GetName().Version.ToString();
+                }
+                return _version;
+            }
+        }
+        private string _version;
+    }
+}
